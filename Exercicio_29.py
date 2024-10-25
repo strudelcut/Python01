432e1f4cff36e9f8954c4b0dcaa75490b17f2225
strudelcut <113107454+strudelcut@users.noreply.github.com>
2024-10-25 09:27:14 -0300

    Add files via upload

--- Exercicio_29.py
+++ Exercicio_29.py
@@ -1,1 +1,13 @@
 # Construa uma página/programa onde o usuário digitará seis números em um vetor e, depois esses números sejam exibidos na tela em ordem digitada.
+
+num = []
+for i in range(6):
+    num.append(int(input(f'Digite o {i+1}° de seis números: ')))
+
+i = 0
+while len(num) > i:
+    print(num[i])
+    i += 1 
+# for i in range(len(num)):
+    # print(num[i])
+print(num)
