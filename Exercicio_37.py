1409922d0c5a886c1eba0059fce73f05c6bd5969
strudelcut <113107454+strudelcut@users.noreply.github.com>
2024-10-29 19:42:36 -0300

    certo

--- Exercicio_37.py
+++ Exercicio_37.py
@@ -1,1 +1,23 @@
-# Construa uma página/programa onde o usuário digitará o nome, o telefone e a cidade de dez pessoas. O programa exbirá, na tela, o nome e o telefone das pessoas que moram em Campo Grande.
+# Construa uma página/programa onde o usuário digitará o nome, o telefone e a cidade de dez pessoas. O programa exbirá, na tela, o nome e o telefone das pessoas que moram em 
+# Campo Grande.
+
+nomes = []
+telefones = []
+cidades = []
+
+for i in range(10):
+    nomes.append(input(f"Digite o {i+1}° nome de dez: "))
+    telefones.append(input(f"Digite o {i+1}° telefone de dez: "))
+    cidades.append(input(f"Digite a {i+1}ª cidade de dez: "))
+
+nomes1 = []
+telefones1 = []
+
+
+for i in range(len(nomes)):
+    if cidades[i] == "Campo Grande":
+        nomes1.append(nomes[i])
+        telefones1.append(telefones[i])
+
+for i in range(len(nomes1)):
+    print(f"Nome: {nomes1[i]}, Telefone: {telefones1[i]}")
