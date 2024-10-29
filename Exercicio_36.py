b047c08b51ba21d4af7bdbc2a2357ad0c8a7a995
strudelcut <113107454+strudelcut@users.noreply.github.com>
2024-10-29 10:35:47 -0300

    Update Exercicio_36.py

--- Exercicio_36.py
+++ Exercicio_36.py
@@ -1,17 +1,20 @@
 # Construa uma página/programa que o usuário digitará o nome e a idade de dez pessoas e o programa escreverá o nome do usuário mais novo
-nomes = [""]
+nomes = []
 idade = []
-comparar = 10000000000
-menor = ''
 
 for i in range(10):
     nomes.append(input(f"Digite o {i+1}° nome de dez: "))
     idade.append(int(input("Digite a idade: ")))
     
+comparar = idade[0]
+menor = nomes[0]
+
 for j in range(len(idade)):
     if(idade[j] < comparar):
         comparar = idade[j]
         menor = nomes[j]
 
 print(f'O usuário mais novo é: {menor}')
+print(nomes)
+print(idade)
         
