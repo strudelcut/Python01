9ce8d0843f7516390926e735da92d8fd71dafa1f
strudelcut <113107454+strudelcut@users.noreply.github.com>
2024-10-30 18:27:18 -0300

    update

--- Funcoes.py
+++ Funcoes.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def Media(n1, n2, n3, n4):
     media = (n1 + n2 + n3 + n4) / 4
     
@@ -19,26 +18,4 @@ def DefineMedia(nome):
 for i in range(5):
     nomeAluno = input("Digite o nome do aluno: ")
     
-=======
-def Media(n1, n2, n3, n4):
-    media = (n1 + n2 + n3 + n4) / 4
-    
-    if(media >= 7):
-        return f"APROVADO, média {media}"
-    elif(media < 7 and media > 4):
-        return f"RECUPERAÇÃO, média {media}"
-    else:
-        return f"REPROVADO, média {media}"
-
-def DefineMedia(nome):    
-    notas = []
-    for i in range(4):
-        notas.append(float(input(f"Digite a {i+1}ª nota: ")))
-
-    print(f"O aluno {nome} está: {Media(notas[0], notas[1], notas[2], notas[3])}")
-
-for i in range(5):
-    nomeAluno = input("Digite o nome do aluno: ")
-    
->>>>>>> 9afa4cdbf03d88c8b2431ba59caea9643357c20a
     DefineMedia(nomeAluno)
