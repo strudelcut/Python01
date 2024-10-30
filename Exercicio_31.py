e031d1f178aa3dfd58e0d756572d4af0604a0654
strudelcut <113107454+strudelcut@users.noreply.github.com>
2024-10-30 18:25:50 -0300

    update

--- Exercicio_31.py
+++ Exercicio_31.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Construa uma página/programa onde o usuário digitará oito números, o programa escreverá na tela qual deles é o maior e qual é o menor.
 
 lista = []
@@ -9,16 +8,4 @@ for i in range(8):
 lista.sort()
 print(f'O menor número: {lista[0]}. O maior número: {lista[len(lista)-1]}')
 # lista.reverse()
-=======
-# Construa uma página/programa onde o usuário digitará oito números, o programa escreverá na tela qual deles é o maior e qual é o menor.
-
-lista = []
-
-for i in range(8):
-    lista.append(int(input(f'Digite o {i+1}° de oito números: ')))
-
-lista.sort()
-print(f'O menor número: {lista[0]}. O maior número: {lista[len(lista)-1]}')
-# lista.reverse()
->>>>>>> 9afa4cdbf03d88c8b2431ba59caea9643357c20a
 # print(f'O maior número: {lista[0]}')
