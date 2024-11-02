e05d470c545fbd78e88f9b39603541eab0258af8
strudelcut <113107454+strudelcut@users.noreply.github.com>
2024-11-01 21:05:34 -0300

    Novas Resoluções

--- Exercicio_35.py
+++ Exercicio_35.py
@@ -20,12 +20,12 @@ def preencheListas():
     for i in range(10):
         alunos.append(input(f"Digite o nome do {i+1}° aluno: "))
         medias.append(float(input(f"Digite o média do {i+1}° aluno: ")))
-        return
+    return
 def retornaMariores():
     for i in range(10):
         if(medias[i] >= 6):
             print(f"aluno {alunos[i]} com média {medias[i]}")
-            return
+    return
         
 preencheListas()
 retornaMariores()
--- Exercicio_37.py
+++ Exercicio_37.py
@@ -1,23 +1,38 @@
 # Construa uma página/programa onde o usuário digitará o nome, o telefone e a cidade de dez pessoas. O programa exbirá, na tela, o nome e o telefone das pessoas que moram em 
 # Campo Grande.
 
+# nomes = []
+# telefones = []
+# bairros = []
+
+# for i in range(3):
+#     nomes.append(input(f"Digite o {i+1}° nome de dez: "))
+#     telefones.append(input(f"Digite o {i+1}° telefone de dez: "))
+#     bairros.append(input(f"Digite a {i+1}ª cidade de dez: "))
+
+# nomes1 = []
+# telefones1 = []
+# bairros1 = []
+
+
+# for i in range(len(nomes)):
+#     if bairros[i] == "Campo Grande":
+#         nomes1.append(nomes[i])
+#         telefones1.append(telefones[i])
+#         bairros1.append(bairros[i])
+
+# for i in range(len(nomes1)):
+#     print(f"Nome: {nomes1[i]}, Telefone: {telefones1[i]}, Bairro: {bairros1[i]}.")
+
 nomes = []
-telefones = []
-cidades = []
+campo = []
 
-for i in range(10):
-    nomes.append(input(f"Digite o {i+1}° nome de dez: "))
-    telefones.append(input(f"Digite o {i+1}° telefone de dez: "))
-    cidades.append(input(f"Digite a {i+1}ª cidade de dez: "))
+for i in range(3):
+    nome = input(f"Digite o nome da {i+1}ª pessoa: ")
+    tel = input(f"Digite o telefone da {i+1}ª pessoa: ")
+    bairro = input(f"Digite o bairro da {i+1}ª pessoa: ")
+    nomes.append({'Nome': nome, 'Telefone': tel, 'Bairro': bairro})
+    if bairro.lower() == 'campo grande':
+        campo.append(nomes[i])
 
-nomes1 = []
-telefones1 = []
-
-
-for i in range(len(nomes)):
-    if cidades[i] == "Campo Grande":
-        nomes1.append(nomes[i])
-        telefones1.append(telefones[i])
-
-for i in range(len(nomes1)):
-    print(f"Nome: {nomes1[i]}, Telefone: {telefones1[i]}")
+print(campo)
